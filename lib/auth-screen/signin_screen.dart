e9c8433ad49b5debc8ebf2cab616a6063b191001
Hina-kazmi42 <zkazmi42@gmail.com>
2024-11-05 08:03:55 +0500

    update

--- lib/auth-screen/signin_screen.dart
+++ lib/auth-screen/signin_screen.dart
@@ -14,9 +14,50 @@ class SigninScreen extends StatefulWidget {
 }
 
 class _SigninScreenState extends State<SigninScreen> {
-  TextEditingController emailController=TextEditingController();
-  TextEditingController passwordController=TextEditingController();
-  bool isLoading=false;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool isLoading = false;
+
+  Future<void> registration() async {
+    isLoading = true;
+    setState(() {
+
+    });
+    await FirebaseAuth.instance.
+    createUserWithEmailAndPassword
+      (email: emailController.text.trim(),
+        password: passwordController.text.trim())
+        .then((value) {
+      isLoading = false;
+      setState(() {
+
+      });
+      Get.defaultDialog(
+
+          content: Icon(Icons.thumb_up, color: Colors.green, size: 20,),
+          title: 'Congratulations',
+          titleStyle: TextStyle(
+            color: Colors.green,
+          )
+      );
+      Get.to(() => LoginScreen());
+    }).onError((error, value) {
+      isLoading = false;
+      setState(() {
+
+      });
+
+      Get.snackbar('Error:', '$error',
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.blue.withOpacity(.5),
+        icon: Icon(Icons.error_outline_outlined, color: Colors.red,),
+
+      );
+    });
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -35,13 +76,22 @@ class _SigninScreenState extends State<SigninScreen> {
           SizedBox(height: 40,),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: TextFormField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  hintText: 'Enter Your Email',
-                  hintStyle: TextStyle(color: Colors.blue),
-                  prefixIcon: Icon(Icons.mail_lock,color: Colors.blue,),
+              child: Container(
+                height: 40,
+                width: 300,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all()
+                ),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Your Email',
+                    hintStyle: TextStyle(color: Colors.blue),
+                    prefixIcon: Icon(Icons.mail_lock,color: Colors.blue,),
+                    border: InputBorder.none
 
+                  ),
                 ),
               ),
             ),
@@ -49,75 +99,50 @@ class _SigninScreenState extends State<SigninScreen> {
 
             Padding(
               padding:  EdgeInsets.symmetric(horizontal: 20),
-              child: TextFormField(
+              child: Container(
+                height: 40,
+                width: 300,
+                decoration:BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: TextFormField(
 
-                controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
 
-                  hintText: 'Enter Your Password',
-                  hintStyle: TextStyle(color: Colors.blue),
-                  prefixIcon: Icon(Icons.password_outlined,color: Colors.blue,),
+                    hintText: 'Enter Your Password',
+                    hintStyle: TextStyle(color: Colors.blue),
+                    prefixIcon: Icon(Icons.password_outlined,color: Colors.blue,),
+                      border: InputBorder.none
 
+                  ),
                 ),
               ),
             ),
             SizedBox(height: 20,),
+            isLoading?CircularProgressIndicator(): InkWell(
+              onTap: (){
+                registration();
 
-          isLoading?CircularProgressIndicator() :InkWell(
-            onTap: ()async{
-              isLoading=true;
-              setState(() {
+              },
 
-              });
+              child: Container(
 
-              await FirebaseAuth.instance.
-              createUserWithEmailAndPassword
-                (email: emailController.text.trim(), password: passwordController.text.trim())
-                  .then((value)
-              {
-                  isLoading=false;
-                  setState(() {
+               height: 40,
+               width: 200,
+               decoration: BoxDecoration(
+                   color: Colors.blue,
+                   borderRadius: BorderRadius.circular(100)
+               ),
+               child: Center(child: Text('Register',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+               ),
+                      ),
+            )
 
-                  });
-                  Get.defaultDialog(
-
-                    content: Icon(Icons.thumb_up,color: Colors.green,size: 20,),
-                    title: 'Congratulations',
-                    titleStyle: TextStyle(
-                      color: Colors.green,
-                    )
-                  );
-                  Get.to(()=>LoginScreen());
-
-
-
-              }).onError((error,value){
-                isLoading=false;
-                setState(() {
-
-                });
-
-                Get.snackbar('Error:', '$error',
-                  snackPosition: SnackPosition.BOTTOM,
-                  backgroundColor: Colors.blue.withOpacity(.5),
-                  icon: Icon(Icons.error_outline_outlined,color: Colors.red,),
-
-                );
-              });
-            },
-            child: Container(
-
-              height: 40,
-              width: 200,
-              decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(100)
-              ),
-              child: Center(child: Text('Register',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
-              ),),
-          ),
-        ],),
+        ],
+    ),
       )
       ,
     );
