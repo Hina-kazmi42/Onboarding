7a01035f7239e637442f2b414a2d17f930aa2bd4
Hina-kazmi42 <zkazmi42@gmail.com>
2024-11-07 02:36:16 +0500

    update

--- lib/starting-screen/splash-screen.dart
+++ lib/starting-screen/splash-screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_gradient/animate_gradient.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
@@ -7,16 +8,39 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
+
+    return Scaffold(
+
+      body: AnimatedSplashScreen(
+
         splash: Center(
-          child: LottieBuilder.asset('lib/assets/animation/Animation2.json')
+
+            child:LottieBuilder.asset('lib/assets/animation/Animation2.json')
+
         )
         , nextScreen:  SigninScreen(),
-      splashIconSize: 3000,
-      duration: 5000,
+        splashIconSize: 3000,
+        duration: 5000,
+        backgroundColor: AnimateGradient(primaryColors: [
+          Colors.green,
+          Colors.deepOrangeAccent,
+          Colors.white,
+        ],
+            secondaryColors: [
+              Colors.white,
+              Colors.orangeAccent,
+              Colors.orange
 
-      backgroundColor: Colors.grey,
+            ]
+        ),
+          ,
 
+
+
+
+
+
+      )
     );
   }
 }
